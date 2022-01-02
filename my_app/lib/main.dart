e6a5bd8e40d837d327c7a48cf293c2d344303b21
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-03 02:02:56 +0530

    Added UI to the app

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -35,18 +35,66 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late Client httpClient;
   late Web3Client ethClient;
-
+  bool data = false;
   final myAdress = '0x4463A5D91fD8Fc0257157663E09285390D124165';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Vx.gray300,
+      backgroundColor: Vx.gray100,
       body: ZStack([
         VxBox()
-            .blue600
+            .gray700
             .size(context.screenWidth, context.percentHeight * 30)
             .make(),
+        VStack([
+          (context.percentHeight * 10).heightBox,
+          "\$PKCOIN".text.xl4.white.bold.center.makeCentered().py16(),
+          (context.percentHeight * 5).heightBox,
+          VxBox(
+                  child: VStack([
+            "Balance".text.black.xl2.semiBold.makeCentered(),
+            10.heightBox,
+            data
+                ? "\$1".text.gray700.semiBold.makeCentered()
+                : CircularProgressIndicator().centered()
+          ]))
+              .p16
+              .white
+              .size(context.screenWidth, context.percentHeight * 18)
+              .rounded
+              .shadowXl
+              .make()
+              .p16(),
+          HStack(
+            [
+              ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.refresh,
+                        color: Colors.white,
+                      ),
+                      label: "Refresh".text.white.make())
+                  .h(50),
+              ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.call_made_outlined,
+                        color: Colors.white,
+                      ),
+                      label: "Deposit".text.white.make())
+                  .h(50),
+              ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.call_received_outlined,
+                          color: Colors.white),
+                      label: "WithDraw".text.white.make())
+                  .h(50)
+            ],
+            alignment: MainAxisAlignment.spaceAround,
+            axisSize: MainAxisSize.max,
+          ),
+        ]),
       ]),
     );
   }
