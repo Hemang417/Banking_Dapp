0fa77416a1d41fba4114119d179067dab4fb60da
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-01-04 11:04:07 +0530

    BlockChain Started

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:web3dart/web3dart.dart';
-import 'slider_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,8 +38,49 @@ class _MyHomePageState extends State<MyHomePage> {
   late Web3Client ethClient;
   bool data = false;
   int myAmount = 0;
-  final myAdress = '0x4463A5D91fD8Fc0257157663E09285390D124165';
+  final myAddress = '0x4463A5D91fD8Fc0257157663E09285390D124165';
   double _currentSliderValue = 20;
+
+  var myData;
+
+  @override
+  void initState() {
+    super.initState();
+    httpClient = Client();
+    ethClient = Web3Client(
+        "https://rinkeby.infura.io/v3/e330d0cda66e4f668082dfea59100c52",
+        httpClient);
+    getBalance(myAddress);
+  }
+
+  Future<DeployedContract> loadContract() async {
+    String abi = await rootBundle.loadString("assets/abi.json");
+    String contactAddress = ":0xd9145CCE52D386f254917e481eB44e9943F39138";
+
+    final contract = DeployedContract(ContractAbi.fromJson(abi, "PKCoin"),
+        EthereumAddress.fromHex(contactAddress));
+
+    return contract;
+  }
+
+  Future<List<dynamic>> query(String functionName, List<dynamic> args) async {
+    final contract = await loadContract();
+    final ethFunction = contract.function(functionName);
+
+    final result = await ethClient.call(
+        contract: contract, function: ethFunction, params: args);
+
+    return result;
+  }
+
+  Future<void> getBalance(String targetAddress) async {
+    // EthereumAddress address = EthereumAddress.fromHex(targetAddress);
+    List<dynamic> result = await query("getBalance", []);
+
+    myData = result[0];
+    data = true;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
             "Balance".text.black.xl2.semiBold.makeCentered(),
             10.heightBox,
             data
-                ? "\$1".text.gray700.semiBold.makeCentered()
+                ? "\$$myData".text.gray700.semiBold.makeCentered()
                 : CircularProgressIndicator().centered()
           ]))
               .p16
@@ -73,12 +114,15 @@ class _MyHomePageState extends State<MyHomePage> {
           Slider(
             value: _currentSliderValue,
             max: 100,
-            divisions: 5,
+            // divisions: 5,
             label: _currentSliderValue.round().toString(),
-            onChanged: (double value) {
-              setState(() {
-                _currentSliderValue = value;
-              });
+            onChanged: (value) {
+              // setState(() {
+              //   _currentSliderValue = value;
+              // });
+
+              myAmount = (value * 100).round();
+              print(myAmount);
             },
           ),
           HStack(
